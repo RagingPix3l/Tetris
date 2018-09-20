1d9809e0bb0180ef0327ca1d10a89f07d85d1e53
Marek Olszewski <marek.stanislaw.olszewski@gmail.com>
2018-09-20 11:30:24 +0200

    changed from detecting collsions to grid based determining if move is possible and such

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,7 @@ _.update = function (g){
     GameObject.prototype.update.call(this,g);
     this.scalep += 0.07;
     this.scale = 0.95 + cos(this.scalep)*0.05;
+
 }
 
 _.draw = function (g, ctx) {
@@ -119,39 +120,6 @@ _.draw = function (g, ctx) {
     GameObjectGrid.prototype.draw.call(me,g,ctx);
 };
 
-_.update = function (g) {
-    const me = this;
-    var oldPos = me.pos.clone();
-    GameObjectGrid.prototype.update.call(me,g);
-
-    if (!me.isGroup){
-        return;
-    }
-
-    if (g.mainGrid.collidesAtSide(me) !== false){
-        me.pos.x = oldPos.x;
-    }
-
-    me.pos.x = clamp(me.pos.x, 0, g.mainGrid.W - me.W);
-    var bMerge = false;
-
-    if (g.mainGrid.shouldMerge(me)){
-        bMerge = true;
-    }
-
-    if (me.pos.y >= g.mainGrid.H  - me.H){
-        bMerge = true;
-    }
-
-    if (bMerge) {
-        me.vel.y = 0;
-        g.mainGrid.merge(me);
-        g.root.remove(me);
-        g.group = g.root.add(g.mainGrid.spawnBrickGroup(g));
-        g.group.pos.x = g.calculateAnchor();
-    }
-};
-
 _.calculatePlacement = function (group) {
   const me = this;
   var row = group.pos.y / (me.brickH+2);
@@ -167,68 +135,9 @@ _.calculatePlacement = function (group) {
             col: col};
 };
 
-_.collidesAtSide = function (group) {
+_.merge = function (group,offscreenGrid) {
     const me = this;
-    const pos = me.calculatePlacement(group);
-    const startRow = pos.row;
-    const startCol = pos.col;
-    var colsToCheck = [0,group.cols-1];
-    for (var i = 0; i < group.rows; ++i){
-        for (var k = 0; k < colsToCheck.length; ++k) {
-            var j = colsToCheck[k];
-
-            if (startCol + j - 1 <= 0){
-                continue;
-            }
-            const srcBrick = group.getAtXY(i,j);
-
-            var dstBrick = me.getAtXY(startRow + i,startCol + j - 1);
-
-            if (dstBrick.solid && srcBrick.solid){
-                return "left";
-            }
-
-            if (startCol + j + 1 >= me.cols){
-                continue;
-            }
-            var dstBrick = me.getAtXY(startRow + i,startCol + j + 1);
-
-            if (dstBrick.solid && srcBrick.solid){
-                return "right";
-            }
-
-        }
-    }
-    return false;
-};
-
-_.shouldMerge = function (group) {
-    const me = this;
-    const pos = me.calculatePlacement(group);
-    const startRow = pos.row;
-    const startCol = pos.col;
-    for (var i = 0; i < group.rows; ++i){
-        for (var j = 0; j < group.cols; ++j) {
-            if (startRow+i+1 >= me.rows){
-                continue;
-            }
-            if (startCol+j >= me.cols){
-                continue;
-            }
-            const dstBrick = me.getAtXY(startRow + i + 1,startCol+j);
-            const srcBrick = group.getAtXY(i,j);
-            if (dstBrick.solid && srcBrick.solid){
-                return true;
-            }
-
-        }
-    }
-    return false;
-};
-
-_.merge = function (group) {
-    const me = this;
-    const pos = me.calculatePlacement(group);
+    const pos = group.lastGridPos;
     const startRow = pos.row;
     const startCol = pos.col;
     var rows = [];
@@ -244,10 +153,10 @@ _.merge = function (group) {
 
         }
     }
-    me.checkFullLines(rows);
+    me.checkFullLines(rows,offscreenGrid);
 };
 
-_.checkFullLines = function (rows) {
+_.checkFullLines = function (rows,offscreenGrid) {
     var me = this;
     rows = rows.sort(function(a,b){
         return a - b;
@@ -341,9 +250,6 @@ _.spawnBrickGroup = function (g) {
     blockGroupGrid.vel.y = 1.5;
     blockGroupGrid.pos.x = g.mainGrid.W*0.5;
 
-    if (me.shouldMerge(blockGroupGrid)){
-        me.isFull = true;
-    };
     return blockGroupGrid;
 }
 
@@ -395,6 +301,41 @@ _.updateSize = function () {
     me.H = me.rows*(me.brickH+2)
 };
 
+_.calculateGroupStartingPosition = function (group,gridOffscreen) {
+    var me = this;
+    var row = 0;
+    var col = (me.cols>>1) - (group.cols>>1);
+    return me.canBePlaced(group,gridOffscreen,row,col) ? {row:row, col:col} : false;
+};
+
+_.canBePlaced = function (group,gridOffscreen,row,col) {
+    var me = this;
+
+    if (row>=gridOffscreen.rows || row<0){
+        return false;
+    }
+    if (col>=gridOffscreen.cols || col<0){
+        return false;
+    }
+
+    for (var i = group.rows - 1; i>=0;--i){
+        for (var j = group.cols - 1; j>=0; --j){
+            if (row + i>=gridOffscreen.rows || row +i <0){
+                return false;
+            }
+            if (col+j>=gridOffscreen.cols || col+j<0){
+                return false;
+            }
+            if (gridOffscreen.getAtXY(i+row,j + col).solid && group.getAtXY(i,j).solid){
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+
+
 function Game (canvasOrcanvasId) {
     var me = this;
     me.canvas = typeof(canvasOrcanvasId) == "string" ? getById(canvasOrcanvasId) : canvasOrcanvasId;
@@ -417,14 +358,14 @@ _.start = function () {
     me.interval = setInterval(function () {
         me.update();
         me.draw();
-    }, 1000/50);
+    }, 1000/ 50);
 };
 
 _.init = function () {
     var me = this;
-    me.mainGrid = me.root.add(new BrickGrid({W:W*0.5,extraRows: 5,bgColor: "rgb(45,165,128)",}));
-    me.group = me.root.add(me.mainGrid.spawnBrickGroup(me));
-    me.group.pos.x = me.calculateAnchor();
+    me.mainGrid = me.root.add(new BrickGrid({W:W*0.5,bgColor: "rgb(45,165,128)",}));
+    me.spawnNewGroup();
+
 };
 
 _.reset = function () {
@@ -434,45 +375,100 @@ _.reset = function () {
     me.init();
 };
 
-_.calculateAnchor = function (){
-    var me = this;
-    return me.mainGrid.calculatePlacement(me.group).col*(me.mainGrid.brickW+me.mainGrid.padding);
+
+_.canBePlaced = function (row,col){
+    const me = this;
+    return me.group.canBePlaced(me.group,me.mainGrid,me.group.lastGridPos.row +row,me.group.lastGridPos.col + col);
 };
 
+_.insert = function () {
+    const me = this;
+    me.mainGrid.merge(me.group,me.gridOffscreen);
+};
+
+_.spawnNewGroup = function () {
+    const me = this;
+
+    me.group = me.root.add(me.mainGrid.spawnBrickGroup(me));
+    const groupStartPos = me.mainGrid.calculateGroupStartingPosition(me.group,me.mainGrid);
+    if (groupStartPos === false){
+        console.log("cannot place group O.o");
+        me.reset();
+    }else{
+        me.group.lastGridPos = groupStartPos;
+        me.updateGroupPos();
+        me.targetPosition = {
+            x: me.group.pos.x,
+            y: me.group.pos.y +(me.mainGrid.brickH+me.mainGrid.padding)
+        };
+        me.group.vel.y = (me.targetPosition.y - me.group.pos.y)/20;
+    }
+}
+_.updateGroupPos = function (){
+    const me = this;
+    me.group.pos.x = me.group.lastGridPos.col*(me.mainGrid.brickW+me.mainGrid.padding);
+    me.group.pos.y = me.group.lastGridPos.row*(me.mainGrid.brickH+me.mainGrid.padding);
+}
 _.update = function () {
     const me = this;
     me.root.update(me);
 
-    if (me.mainGrid.isFull){
-        me.reset();
+    if (me.keyboard.keys['ArrowDown']){
+        me.group.vel.y = clamp(me.group.vel.y * 1.5, 0, 15);
     }
 
-    const anchor = me.calculateAnchor();
+    if (me.keyboard.keys['Space']){
+        me.keyboard.keys['Space'] = false;
+        me.group.rotate(1);
 
+        if (!me.canBePlaced(0,0)){
+            me.group.rotate(3);
+        }
+
+    }
+
+    if (me.group.pos.y >= me.targetPosition.y){
+        me.group.pos.y = me.targetPosition.y;
+        if (me.canBePlaced(1,0)){
+            me.group.lastGridPos.row ++;
+            me.targetPosition.y = (me.group.lastGridPos.row+(me.canBePlaced(1,0)?1:0)) * (me.mainGrid.brickH+me.mainGrid.padding);
+        } else {
+            me.insert();
+            me.root.remove(me.group);
+            me.spawnNewGroup();
+        }
+        //me.updateGroupPos();
+
+
+    }
     if (me.group.vel.x == 0){
-        me.group.startPosX=anchor;
-    }
-
-    if (me.keyboard.keys['ArrowLeft'] || me.keyboard.keys['ArrowRight']){
         if (me.keyboard.keys['ArrowLeft']){
-            me.group.vel.x = -6;
-        }else{
-            me.group.vel.x = 6;
+            if (me.canBePlaced(1,-1)){
+                me.targetPosition.x = (me.group.lastGridPos.col-(me.canBePlaced(1,-1)?1:0)) * (me.mainGrid.brickW+me.mainGrid.padding)
+                me.group.vel.x = (me.targetPosition.x - me.group.pos.x)/10;
+            }
+        }else if (me.keyboard.keys['ArrowRight']) {
+            if (me.canBePlaced(1, 1)) {
+                me.targetPosition.x = (me.group.lastGridPos.col + (me.canBePlaced(1, 1) ? 1 : 0)) * (me.mainGrid.brickW + me.mainGrid.padding)
+                me.group.vel.x = (me.targetPosition.x - me.group.pos.x) / 10;
+            }
         }
     }else{
-        if (me.group.startPosX != anchor) {
-            me.group.vel.x = 0;
-            me.group.pos.x = anchor;
+        if (me.group.vel.x < 0){
+            if (me.group.pos.x <= me.targetPosition.x){
+                me.group.vel.x = 0;
+                me.group.lastGridPos.col --;
+                me.group.pos.x = me.targetPosition.x;
+            }
+        }else if (me.group.vel.x > 0){
+            if (me.group.pos.x >= me.targetPosition.x){
+                me.group.vel.x = 0;
+                me.group.lastGridPos.col ++;
+                me.group.pos.x = me.targetPosition.x;
+            }
         }
     }
-    if (me.keyboard.keys['Space'] ||me.keyboard.keys['ArrowUp'] ){
-        me.group.rotate(1);
-        me.keyboard.keys['Space'] = me.keyboard.keys['ArrowUp'] = false;
-    }
-    if (me.keyboard.keys['ArrowDown']){
-        me.group.vel.y*=2.5;
-        me.keyboard.keys['ArrowDown'] = false;
-    }
+
 
 };
 
